8a88fb52bbf6e4c75c6c70d52651e86d3cd7a424
JustineRgs <108766192+JustineRgs@users.noreply.github.com>
2022-10-27 10:30:06 +0200

    day 3 - p6

--- js/burger.js
+++ js/burger.js
@@ -9,7 +9,7 @@ const main = document.querySelector("main");
 const footer = document.querySelector("footer");
 
 //On crée un évènement sur le clic du bouton burger pour afficher le menu
-burgerBouton.addEventListener("click", () => {
+burgerBouton.addEventListener("touchstart", () => {
     if(burgerMenu.style.visibility === "hidden"){
         burgerMenu.style.visibility = "visible";
     }
@@ -19,10 +19,10 @@ burgerBouton.addEventListener("click", () => {
 });
 
 //On crée un évènement pour pouvoir fermer le menu en cliquant n'importe où sur l'écran
-main.addEventListener("click", () => {
+main.addEventListener("touchstart", () => {
     burgerMenu.style.visibility = "hidden"; 
 });
 
-footer.addEventListener("click", () => {
+footer.addEventListener("touchstart", () => {
     burgerMenu.style.visibility = "hidden"; 
 });
