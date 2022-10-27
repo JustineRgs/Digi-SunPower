8fe95e51ad60239cfcdf6891f962719e7faa4a61
JustineRgs <108766192+JustineRgs@users.noreply.github.com>
2022-10-27 10:23:31 +0200

    day 3 - p4

--- Js/active.js
+++ Js/active.js
@@ -1,17 +1,16 @@
-// var itemContainer = document.getElementById("navbar_dekstop");
+var itemContainer = document.getElementById("navbar_dekstop");
 
-// var item = itemContainer.getElementsByClassName("navbar_item");
+var item = itemContainer.getElementsByClassName("navbar_item");
 
-// for (var i = 0; i < item.length; i++) {
-//   item[i].addEventListener("click", function() {
-//     var current = document.getElementsByClassName("active");
+for (var i = 0; i < item.length; i++) {
+  item[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
 
-//   if (current.length > 0) {
-//     current[0].className = current[0].className.replace("active", "");
-//   }
+  if (current.length > 0) {
+    current[0].className = current[0].className.replace("active", "");
+  }
 
-//   // Add the active class to the current/clicked button
-//   this.className += " active";
-// });
-// }
+  this.className += " active";
+});
+}
   
