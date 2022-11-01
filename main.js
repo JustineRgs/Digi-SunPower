6c3b8ce05a17a786eaa4f5799f9417599e6164ec
JustineRgs <108766192+JustineRgs@users.noreply.github.com>
2022-11-01 22:39:52 +0100

    day 8 - last push acc

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const burgerBouton = document.getElementById("nav_bar_burger");
 
 // On déclare une variable correspondant à l'élément du DOM du menu burger
-const burgerMenu = document.getElementById("navbar_dekstop")
+// const burgerMenu = document.getElementsByClassName("navbar_dekstop");
+const burgerMenu = document.querySelector(".navbar_dekstop");
 
 //On déclare des variables pointant sur le main et le footer
 const main = document.querySelector("main");
@@ -10,19 +11,19 @@ const footer = document.querySelector("footer");
 
 //On crée un évènement sur le clic du bouton burger pour afficher le menu
 burgerBouton.addEventListener("click", () => {
-    if(burgerMenu.style.visibility === "hidden"){
-        burgerMenu.style.visibility = "visible";
+    if(burgerMenu.classList.contains("transition_nav")){
+        burgerMenu.classList.remove("transition_nav"); 
     }
     else{
-        burgerMenu.style.visibility = "hidden";    
+        burgerMenu.classList.add("transition_nav");
     }
 });
 
 //On crée un évènement pour pouvoir fermer le menu en cliquant n'importe où sur l'écran
 main.addEventListener("click", () => {
-    burgerMenu.style.visibility = "hidden"; 
+    burgerMenu.classList.remove("transition_nav");
 });
 
 footer.addEventListener("click", () => {
-    burgerMenu.style.visibility = "hidden"; 
+    burgerMenu.classList.remove("transition_nav");
 });
