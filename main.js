1cacf607189b8e9a9ed6621acc4f7e151a1cdda2
justinergs <justineragues@gmail.com>
2024-01-11 23:09:38 +0100

    fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //-------------- BURGER MENU --------------
 
 // On déclare une variable correspondant à l'élément du DOM du bouton burger
-const burgerBouton = document.getElementById("nav_bar_burger");
+const burgerBouton = document.querySelector(".nav_bar_burger");
 
 // On déclare une variable correspondant à l'élément du DOM du menu burger
 // const burgerMenu = document.getElementsByClassName("navbar_dekstop");
@@ -13,20 +13,18 @@ const footer = document.querySelector("footer");
 
 //On crée un évènement sur le clic du bouton burger pour afficher le menu
 burgerBouton.addEventListener("click", () => {
-    if(burgerMenu.classList.contains("transition_nav")){
-        burgerMenu.classList.remove("transition_nav"); 
-    }
-    else{
-        burgerMenu.classList.add("transition_nav");
-    }
+  if (burgerMenu.classList.contains("transition_nav")) {
+    burgerMenu.classList.remove("transition_nav");
+  } else {
+    burgerMenu.classList.add("transition_nav");
+  }
 });
 
 //On crée un évènement pour pouvoir fermer le menu en cliquant n'importe où sur l'écran
 main.addEventListener("click", () => {
-    burgerMenu.classList.remove("transition_nav");
+  burgerMenu.classList.remove("transition_nav");
 });
 
 footer.addEventListener("click", () => {
-    burgerMenu.classList.remove("transition_nav");
+  burgerMenu.classList.remove("transition_nav");
 });
-
